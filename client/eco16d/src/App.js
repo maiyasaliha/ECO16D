465cebfa4a88813e5a19993726de65dc8e54724b
maiyasaliha <s.maiya095@gmail.com>
2024-05-28 09:43:59 +0800

    attempt at integrating quill to ag grid

--- client/eco16d/src/App.js
+++ client/eco16d/src/App.js
@@ -1,10 +1,11 @@
 import Toolbar from "./components/Toolbar";
 import Spreadsheet from "./components/Spreadsheet";
+import QuillSpreadsheet from "./components/QuillSpreadsheet";
 
 function App() {
   return (
     <div >
-      <Spreadsheet />
+      <QuillSpreadsheet />
     </div>
   );
 }
